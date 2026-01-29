5454673d7438cd12a62e007ce2113a46132d08ed
Anmol-kam <singjila5@gmail.com>
2026-01-29 12:55:39 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,110 @@
+
+// 1
+// for(i=1; i<=5; i++){
+//     bag=""
+//     for(j=1; j<=5; j++){
+//         bag+="*"
+//     }
+//     console.log(bag);
+// }
+
+
+
+// 2
+// for( i=1; i<=5; i++){
+//     bag=""
+//     for(j=1; j<=i; j++){
+//         bag=bag+"*"
+//     }
+//     console.log(bag);
+// }
+
+
+// 3
+//  for( i=1; i<=5; i++){
+//     bag=""
+//     for(j=5; j>=i; j--){
+//         bag=bag+"*"
+//     }
+//     console.log(bag);
+// }
+
+
+
+
+
+
+// 4
+// for( i=1; i<=5; i++){
+//     bag=""
+//     for(j=1; j<=i; j++){
+//         bag=bag+j  
+//     }
+//     console.log(bag);
+// }
+
+5
+
+// for(i=1; i<=5; i++){
+//     bag=""
+
+//     for(j=1; j<=5-i; j++){
+//         bag=bag+" "
+//     }
+
+//     for(k=1; k<=2*i-1;k++){
+//         bag=bag+"*"
+//     }
+//     console.log(bag);
+// }
+
+
+
+
+// 6
+
+// for(i=1; i<=5; i++){
+//     bag=""
+//     for(j=1; j<=i; j++){
+//         bag=bag+i
+//     }
+//     console.log(bag);
+// }
+
+
+// 7
+
+// for(i=1; i<=5; i++){
+//     bag=""
+
+//     for(j=1; j<=5-i; j++){
+//         bag=bag+" "
+//     }
+
+//     for(k=1; k<=2*i-1;k++){
+//         bag=bag+"*"
+//     }
+//     console.log(bag);
+// }
+// for(i=4; i>=1; i--){
+//     bag=""
+
+//     for(j=1; j<=5-i; j++){
+//         bag=bag+" "
+//     }
+
+//     for(k=1; k<=2*i-1;k++){
+//         bag=bag+"*"
+//     }
+//     console.log(bag);
+// }
+
+
+8
+for(i=5; i<=i; i--){
+    bag=""
+    for(j=i; j>=1; j--){
+        bag=bag+j
+    }
+    console.log(bag);
+}
