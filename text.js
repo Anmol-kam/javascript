36e7cdc08b7d0a5d89504c6088f97cd25b12fe8f
Anmol-kam <singjila5@gmail.com>
2026-02-02 12:40:00 +0530

    Add files via upload

--- text.js
+++ text.js
@@ -1,110 +1,153 @@
-
 // 1
-// for(i=1; i<=5; i++){
-//     bag=""
-//     for(j=1; j<=5; j++){
-//         bag+="*"
-//     }
-//     console.log(bag);
-// }
 
+
+// let score = 59;
+// let grade;
+
+
+// switch (true) {
+//     case score >= 90 && score <= 100:
+
+//         grade = "A"
+//         break;
+
+//     case score >= 80 && score <= 89:
+
+//         grade = "B"
+//         break;
+
+//     case score >= 70 && score <= 79:
+
+//         grade = "c"
+//         break;
+
+
+//     case score >= 60 && score <= 69:
+
+//         grade = "D"
+//         break;
+
+//     default :
+
+//         grade = "F"
+
+// }
+// console.log(grade);
 
 
 // 2
-// for( i=1; i<=5; i++){
-//     bag=""
-//     for(j=1; j<=i; j++){
-//         bag=bag+"*"
-//     }
-//     console.log(bag);
+
+// let num = 1;
+// switch (num) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+
+//     case 4:
+//         console.log("Thursday");
+//         break;
+
+//     case 5:
+//         console.log("Friday");
+//         break;
+
+//     case 6:
+//         console.log("Saturday");
+//         break;
+
+
+//     case 7:
+//         console.log("Sunday");
+//         break;
+
+//     default:
+//         console.log("invalid");
 // }
 
 
 // 3
-//  for( i=1; i<=5; i++){
-//     bag=""
-//     for(j=5; j>=i; j--){
-//         bag=bag+"*"
-//     }
-//     console.log(bag);
+
+// let num = 12;
+// switch (num) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+
+//     case 3:
+//         console.log("March");
+//         break;
+
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     case 5:
+//         console.log("May");
+//         break;
+
+//     case 6:
+//         console.log("June");
+//         break;
+
+
+//     case 7:
+//         console.log("July");
+//         break;
+
+//      case 8:
+//         console.log("August");
+//         break;
+
+//     case 9:
+//         console.log("September");
+//         break;
+
+//     case 10:
+//         console.log("October");
+//         break;
+
+//     case 11:
+//         console.log("November");
+//         break;
+
+
+//     case 12:
+//         console.log("December");
+//         break;
+
+//         default:
+//             console.log("invalid");
 // }
-
-
-
 
 
 
 // 4
-// for( i=1; i<=5; i++){
-//     bag=""
-//     for(j=1; j<=i; j++){
-//         bag=bag+j  
-//     }
-//     console.log(bag);
-// }
+// let colour = "green";
+// let rule;
+// switch (colour) {
+//     case "red":
+//         rule = "stop"
+//         break;
 
-5
+//     case "green":
+//         rule = "Go"
+//         break;
+//     case "yellow":
+//         rule = "slow down"
+//         break;
+//     default:
+//         rule = "invalid"
 
-// for(i=1; i<=5; i++){
-//     bag=""
-
-//     for(j=1; j<=5-i; j++){
-//         bag=bag+" "
-//     }
-
-//     for(k=1; k<=2*i-1;k++){
-//         bag=bag+"*"
-//     }
-//     console.log(bag);
-// }
+// }console.log(rule);
 
 
-
-
-// 6
-
-// for(i=1; i<=5; i++){
-//     bag=""
-//     for(j=1; j<=i; j++){
-//         bag=bag+i
-//     }
-//     console.log(bag);
-// }
-
-
-// 7
-
-// for(i=1; i<=5; i++){
-//     bag=""
-
-//     for(j=1; j<=5-i; j++){
-//         bag=bag+" "
-//     }
-
-//     for(k=1; k<=2*i-1;k++){
-//         bag=bag+"*"
-//     }
-//     console.log(bag);
-// }
-// for(i=4; i>=1; i--){
-//     bag=""
-
-//     for(j=1; j<=5-i; j++){
-//         bag=bag+" "
-//     }
-
-//     for(k=1; k<=2*i-1;k++){
-//         bag=bag+"*"
-//     }
-//     console.log(bag);
-// }
-
-
-8
-for(i=5; i<=i; i--){
-    bag=""
-    for(j=i; j>=1; j--){
-        bag=bag+j
-    }
-    console.log(bag);
-}
