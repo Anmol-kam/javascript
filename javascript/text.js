48d29cc453b6c7b0d6b99dd28e3648ca96ce9353
Anmol-kam <singjila5@gmail.com>
2026-02-03 12:36:40 +0530

    gvfdb

---
+++ javascript/text.js
@@ -0,0 +1,306 @@
+// 1
+
+
+// let score = 59;
+// let grade;
+
+
+// switch (true) {
+//     case score >= 90 && score <= 100:
+
+//         grade = "A"
+//         break;
+
+//     case score >= 80 && score <= 89:
+
+//         grade = "B"
+//         break;
+
+//     case score >= 70 && score <= 79:
+
+//         grade = "c"
+//         break;
+
+
+//     case score >= 60 && score <= 69:
+
+//         grade = "D"
+//         break;
+
+//     default :
+
+//         grade = "F"
+
+// }
+// console.log(grade);
+
+
+// 2
+
+// let num = 1;
+// switch (num) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+
+//     case 4:
+//         console.log("Thursday");
+//         break;
+
+//     case 5:
+//         console.log("Friday");
+//         break;
+
+//     case 6:
+//         console.log("Saturday");
+//         break;
+
+
+//     case 7:
+//         console.log("Sunday");
+//         break;
+
+//     default:
+//         console.log("invalid");
+// }
+
+
+// 3
+
+// let num = 12;
+// switch (num) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+
+//     case 3:
+//         console.log("March");
+//         break;
+
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     case 5:
+//         console.log("May");
+//         break;
+
+//     case 6:
+//         console.log("June");
+//         break;
+
+
+//     case 7:
+//         console.log("July");
+//         break;
+
+//      case 8:
+//         console.log("August");
+//         break;
+
+//     case 9:
+//         console.log("September");
+//         break;
+
+//     case 10:
+//         console.log("October");
+//         break;
+
+//     case 11:
+//         console.log("November");
+//         break;
+
+
+//     case 12:
+//         console.log("December");
+//         break;
+
+//         default:
+//             console.log("invalid");
+// }
+
+
+
+// 4
+// let colour = "green";
+// let rule;
+// switch (colour) {
+//     case "red":
+//         rule = "stop"
+//         break;
+
+//     case "green":
+//         rule = "Go"
+//         break;
+//     case "yellow":
+//         rule = "slow down"
+//         break;
+//     default:
+//         rule = "invalid"
+
+// }console.log(rule);
+
+
+// 5
+// let a=16;
+// let b=8;
+// let operator ="/"
+// let c
+// switch(operator){
+//     case operator="+":
+//         c=a+b
+//         break;
+
+//         case operator="-":
+//             c=a-b
+//             break;
+
+//             case operator="*":
+//             c=a*b
+//             break;
+
+//             case operator="/":
+//             c=a/b
+//             break;
+
+//             default:
+//                 c="invalid"
+
+
+// }console.log(c);
+
+
+// 6
+// let day = 9;
+// switch (day) {
+//     case 1:
+
+//     case 2:
+
+//     case 3:
+
+//     case 4:
+
+//     case 5:
+
+//         console.log("It i2s weekdays");
+//         break;
+
+//     case 6:
+
+//     case 7:
+
+//         console.log("It is weeekend");
+//         break;
+
+//     default:
+//         console.log("invalid");
+// }
+
+
+// 7
+
+// let number = 3;
+// let season
+// switch (true) {
+//     case number == 1 && number == 2:
+//         season = "winter"
+//         break;
+
+//     case number <= 5 && number >= 3:
+//         season = "spring"
+//         break;
+
+
+//     case number >= 6 && number <= 8:
+//         season = "summer"
+//         break;
+
+//     case number >= 9 && number <= 11:
+//         season = "fall"
+//         break;
+
+//     case number == 12:
+//         season = "winter"
+//         break;
+
+//     default:
+//         season = "invalid season"
+// } console.log(season);             
+
+
+// 8
+
+// let score = 97;
+// let grade
+// switch (true) {
+//     case score >= 90:
+//         grade = "excelent"
+//         break;
+
+//     case score >= 70 && score <= 89:
+//         grade = "Good"
+//         break;
+
+//     case score < 70:
+//         grade = "Needs Improvement"
+//         break;
+
+//     default:
+//         grade = "fail"
+
+// }console.log(grade);
+
+
+// 9
+// let age = 45;
+// let A
+// switch (true) {
+//     case age <= 12:
+//         A = "child"
+//         break;
+
+//     case age >= 13 && age <= 19:
+//         A = "Teenager"
+//         break;
+
+//     case age >= 20 && age <= 35:
+//         A = "Young Adult"
+//         break;
+
+//     case age >= 36 && age <= 60:
+//         A = "Adult"
+//         break;
+//     case age > 60:
+//         A = "Senior"
+//         break;
+
+//     default:
+//         A = "invalid"
+// } console.log(A);
+
+
+// 10
+// let toss = 5;
+// switch (toss) {
+//     case 1:
+//         console.log("Head");
+//         break;
+
+//     case 2:
+//         console.log("Tail");
+//         break;
+
+//     default:
+//         console.log("invalid");
+// }
