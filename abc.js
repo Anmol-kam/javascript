0d9483b14db0e3143a71a25c8a0baab8b5eeae73
Anmol-kam <singjila5@gmail.com>
2026-02-05 12:16:05 +0530

    nhcv

---
+++ abc.js
@@ -0,0 +1,39 @@
+// 1
+
+// for( i=1; i<=100; i++){
+//     console.log(i);
+// }
+
+
+// 2
+// for(i=1; i<=10; i++){
+//     console.log(i*i);
+// }
+
+
+// 3
+
+// let sum =0;
+// for(i=1; i<=50; i++){
+//     if(i%2!==0){
+//         sum=sum+i;
+       
+//     }
+     
+// }console.log(sum);
+
+// 4
+// for( i=100; i>=1; i--){
+//     console.log(i);
+// }
+
+
+// 5
+// let num=12;
+// for(i=1; i<=num; i++){
+//     if(num%i==0){
+//         console.log(i);
+//     }
+
+// }
+
