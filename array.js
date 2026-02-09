137177ec98328d2ddb7b271bfd99a8bc80c8c12d
Anmol-kam <singjila5@gmail.com>
2026-02-09 12:29:19 +0530

    ytrur

---
+++ array.js
@@ -0,0 +1,52 @@
+
+// 1
+// let a=[1,2,3,4];
+
+
+// 2
+// let arr=[5,10,15];
+// console.log(arr[1]);
+
+// 3
+// let arr=[1,2,3,4];
+// console.log(arr.length);
+
+// 4
+// let arr=[1,2];
+// arr.push(3)
+// console.log(arr);
+
+
+// 5
+// let arr=[1,2,3];
+// arr.pop()
+// console.log(arr);
+
+
+// 6
+// let a=[2,4,6];
+// let b="javascript"
+// console.log(Array.isArray(a) );
+
+// 7
+// let arr=[10,20,30,40];
+// arr.shift()
+// console.log(arr);
+
+
+// 8
+// let a=[1,4,6,7]
+// a.unshift(5)
+// console.log(a);
+
+
+// 9
+// let fruits = ['apple', 'banana', 'cherry', 'date']
+// fruits.splice(1,2)
+// console.log(fruits);
+
+
+// 10
+// let fruits = ['apple', 'banana',  'date']
+// fruits.splice(2,0,'cherry')
+// console.log(fruits);
